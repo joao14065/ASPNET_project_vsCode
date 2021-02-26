278e8d0841de52ef830476fde00ad79663ee9a3e
Joao Paulo <joaopaulorpo@gmail.com>
2021-02-26 09:19:06 -0300

    JOAO - Starting the project

---
+++ ASPNET_API_proj/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ASPNET_API_proj.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+           
+           return BadRequest("Invalid input");
+        }
+    }
+}
